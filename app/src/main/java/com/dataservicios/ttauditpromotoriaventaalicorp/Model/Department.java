e63648b064f59adefe210703e6f303277ae3233a
Jaime <jcdiaz356@hotmasil.com>
2017-07-20 11:44:02 -0500

    20-07-2017

---
+++ app/src/main/java/com/dataservicios/ttauditpromotoriaventaalicorp/Model/Department.java
@@ -0,0 +1,10 @@
+package com.dataservicios.ttauditpromotoriaventaalicorp.Model;
+
+/**
+ * Created by jcdia on 23/03/2017.
+ */
+
+public class Departamento {
+    private int id;
+    private String name;
+}
---
+++ app/src/main/java/com/dataservicios/ttauditpromotoriaventaalicorp/Model/District.java
@@ -0,0 +1,8 @@
+package com.dataservicios.ttauditpromotoriaventaalicorp.Model;
+
+/**
+ * Created by jcdia on 23/03/2017.
+ */
+
+public class Distrito {
+}
